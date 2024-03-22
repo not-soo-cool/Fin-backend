a7321f2f90413da41e484ce79c55140dddc351e6
not-soo-cool <yashvendrasengar1111@gmail.com>
2024-03-22 20:49:25 +0530

    seventh commit

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from 'express'
 import { adminRouter } from './routes/adminRoute.js';
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from "dotenv";
 import { investorRouter } from './routes/investorRoute.js';
 import { customerRouter } from './routes/customerRoute.js';
 
 export const app = express();
+
+dotenv.config({ path: "./config/config.env" })
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({
@@ -21,6 +24,8 @@ app.use(
       credentials: true,
     })
 );
+
+console.log(`WEB URL: ${process.env.WEB_URL}`);
 
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/investor", investorRouter);

64fa70f02a0a1486ec4021a556f6acea9dd418d6
not-soo-cool <yashvendrasengar1111@gmail.com>
2024-04-16 02:50:11 +0530

    Instal

--- models/instalmentModel.js
+++ models/instalmentModel.js
@@ -44,12 +44,12 @@ instalSchema.pre('save', async function(next) {
     try {
         if (this.isNew) {
             let lastInstalment = await this.constructor.findOne({}, {}, { sort: { 'ID': -1 } });
-            let lastID = "FIN001"; // Default ID if no instalments exist yet
+            let lastID = "FIN0001"; // Default ID if no instalments exist yet
 
             if (lastInstalment) {
                 // Extract the numeric part of the last ID, increment it by 1, and pad it with zeros
                 const lastIDNumber = parseInt(lastInstalment.ID.substring(3));
-                lastID = `FIN${(lastIDNumber + 1).toString().padStart(3, '0')}`;
+                lastID = `FIN${(lastIDNumber + 1).toString().padStart(4, '0')}`;
             }
 
             // Set the generated ID for the new instalment
